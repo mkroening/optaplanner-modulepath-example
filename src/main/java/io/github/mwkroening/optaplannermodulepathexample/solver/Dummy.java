34a23413bd6635f1d9b1291fba37feab866a444e
Martin Kröning <m.kroening@hotmail.de>
2019-03-14 11:30:08 +0100

    Remove the dummy class

--- src/main/java/io/github/mwkroening/optaplannermodulepathexample/solver/Dummy.java
+++
@@ -1,7 +0,0 @@
-package io.github.mwkroening.optaplannermodulepathexample.solver;
-
-// Currently required for opening this package.
-// Probably an issue with the module plugin
-public class Dummy {
-
-}
