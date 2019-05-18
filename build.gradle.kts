1106eeeb9724b805fdf6858b888e24cb889fbf60
Martin Kröning <m.kroening@hotmail.de>
2019-05-18 22:15:26 +0200

    Try to make a jlink image

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     id("org.javamodularity.moduleplugin") version "1.5.0"
+    id("org.beryx.jlink") version "2.10.3"
 }
 
 patchModules.config = listOf(
@@ -9,6 +10,55 @@ patchModules.config = listOf(
         // See https://github.com/x-stream/xstream/issues/120
         "xmlpull=xpp3_min-1.1.4c.jar"
 )
+
+jlink {
+    addOptions("--bind-services")
+
+    // based on `./gradlew suggestMergedModuleInfo --language=kotlin`
+    mergedModule {
+        requires("java.management")
+        requires("java.naming")
+        requires("java.logging")
+        requires("java.security.sasl")
+        requires("java.scripting")
+        requires("java.sql")
+        requires("jdk.jdi")
+        requires("java.xml")
+        requires("java.desktop")
+        requires("java.datatransfer")
+        requires("java.compiler")
+        requires("jdk.unsupported")
+        uses("javax.annotation.processing.Processor")
+        uses("org.kie.internal.runtime.conf.ObjectModelResolver")
+
+        // PatternCompiler is not public in com.google.common.base; cannot be accessed from outside package
+        // uses("com.google.common.base.PatternCompiler");
+
+        // the service implementation type must be a subtype of the service interface type, or have a public static no-args method named "provider" returning the service implementation
+        // provides("org.xmlpull.v1.XmlPullParserFactory").with("org.xmlpull.mxp1.MXParser,org.xmlpull.mxp1_serializer.MXSerializer");
+
+        provides("javax.script.ScriptEngineFactory").with("org.mvel2.jsr223.MvelScriptEngineFactory")
+        provides("javax.tools.JavaCompiler").with("org.eclipse.jdt.internal.compiler.tool.EclipseCompiler")
+
+        // Manual additions
+        requires("org.slf4j")
+    }
+
+    launcher {
+        jvmArgs = listOf(
+                // Drools requires to access java.lang.ClassLoader.defineClass
+                "--add-opens", "java.base/java.lang=optaplanner.modulepath.example.merged.module",
+
+                // XStream requires us to manually open up the JDK
+                // See https://github.com/x-stream/xstream/issues/101
+                "--add-opens", "java.base/java.util=optaplanner.modulepath.example.merged.module",
+                "--add-opens", "java.base/java.lang.reflect=optaplanner.modulepath.example.merged.module",
+                "--add-opens", "java.base/java.text=optaplanner.modulepath.example.merged.module",
+                "--add-opens", "java.desktop/java.awt.font=optaplanner.modulepath.example.merged.module"
+        )
+    }
+}
+
 
 application {
     mainClassName = "$moduleName/io.github.mwkroening.optaplannermodulepathexample.App"
